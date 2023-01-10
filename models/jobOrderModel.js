602bd08425b2c3c92119ba396df6568594e81e59
jerryclarkc0420 <jerryclarkc0420@gmail.com>
2023-01-10 22:30:45 +0800

    Created model schema

--- models/jobOrderModel.js
+++ models/jobOrderModel.js
@@ -4,15 +4,122 @@ const Schema = mongoose.Schema;
 
 //task schema
 const jobOrderDBSchema = new Schema({
-    task: {
+    
+    //Customer information
+    job_id: {
         type: String,
         required: true
     },
-    status: {
+    job_date: {
         type: String,
         required: true
-    }
-    
+    },
+    cus_name: {
+        type: String,
+        required: true
+    },
+    cus_address: {
+        type: String,
+        required: true
+    },
+    cus_phone: {
+        type: String,
+        required: true
+    },
+    //Unit Information
+    unit_model: {
+        type: String,
+        required: false,
+        default: "No Model"
+    },
+    unit_specs: {
+        type: String,
+        required: false,
+        default: "No Specs"
+    },
+    unit_accessories: {
+        type: String,
+        required: false,
+        default: "No Accessories"
+    },
+    work_perf: {
+        type: String,
+        required: true
+    },
+    //Job payment info s = service
+    s_charge: {
+        type: Number,
+        required: true
+    },
+    s_paymeth: {
+        type: String,
+        required: true
+    },
+    s_downpay: {
+        type: Number,
+        required: true
+    },
+    s_bal: {
+        type: Number,
+        required: true
+    },
+    s_status: {
+        type: String,
+        required: true
+    },
+    //Parts Order info p = parts
+    p_parts: {
+        type: String,
+        required: false,
+        default: "Not Applicable"
+    },
+    p_ord_date: {
+        type: String,
+        required: false,
+        default: "Not Applicable"
+    },
+    p_supp: {
+        type: String,
+        required: false,
+        default: "Not Applicable"
+    },
+    p_price: {
+        type: Number,
+        required: false,
+        default: 0
+    },
+    p_ord_status: {
+        type: String,
+        required: false,
+        default: "Not Applicable"
+    },
+    p_installed: {
+        type: String,
+        required: false,
+        default: "Not Applicable"
+    },
+    //parts payment
+    p_downpay: {
+        type: Number,
+        required: false,
+        default: 0
+    },
+    p_bal: {
+        type: Number,
+        required: false,
+        default: 0
+    },
+    p_status: {
+        type: String,
+        required: false,
+        default: "Not Applicable"
+    },
+    p_rel_date: {
+        type: String,
+        required: false,
+        default: "Not Applicable"
+    },
+
 }, {timestamps: true});
 
 //create task model
