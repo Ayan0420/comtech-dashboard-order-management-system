4ef85d4f3fb1ac0609531f68b6dc8d4c40c48a32
jerryclarkc0420 <jerryclarkc0420@gmail.com>
2023-04-17 15:56:04 +0800

    implemented annual sales chart

--- controllers/dashboardFunctions.js
+++ controllers/dashboardFunctions.js
@@ -10,7 +10,7 @@ const dashboard = (req, res) => {
             let monthNowName = months[dateNow.getMonth()];
             
             yearNow = dateNow.getFullYear();
-            console.log('yearNow', yearNow)
+            // console.log('yearNow', yearNow)
                 // console.log("Date Now Object: " + dateNow)
                 // console.log("Month Today: " + monthNow)
                 // console.log("Year Today: " + yearNow)
@@ -21,7 +21,13 @@ const dashboard = (req, res) => {
             //this for loop is used to append filtered data by current month to the dataByCurrentMonth array.
             for(let i = 0; i < data.length; i++){
                 let month = data[i].job_date.substring(5, 7); //removing 2023- and -11 from 2023-02-11 leaving only 02.
-                
+                let salesYear = data[i].job_date.substring(0, 4);
+
+                //to fetch sales only from current year
+                if(salesYear != yearNow){
+                    continue
+                } 
+
                 if (month != monthNow){
                     continue;
                 } else {
@@ -171,29 +177,29 @@ const dashboard = (req, res) => {
 
                     //to append data for each month
                     if(salesMonth === '01'){
-                        janSales.push(data[i].s_downpay + data[i].p_downpay);
+                        janSales.push(data[i].s_downpay);
                     } else if(salesMonth === '02'){
-                        febSales.push(data[i].s_downpay + data[i].p_downpay);
+                        febSales.push(data[i].s_downpay);
                     } else if(salesMonth === '03'){
-                        marSales.push(data[i].s_downpay + data[i].p_downpay);
+                        marSales.push(data[i].s_downpay);
                     } else if(salesMonth === '04'){
-                        aprSales.push(data[i].s_downpay + data[i].p_downpay);
+                        aprSales.push(data[i].s_downpay);
                     } else if(salesMonth === '05'){
-                        maySales.push(data[i].s_downpay + data[i].p_downpay);
+                        maySales.push(data[i].s_downpay);
                     } else if(salesMonth === '06'){
-                        junSales.push(data[i].s_downpay + data[i].p_downpay);
+                        junSales.push(data[i].s_downpay);
                     } else if(salesMonth === '07'){
-                        julSales.push(data[i].s_downpay + data[i].p_downpay);
+                        julSales.push(data[i].s_downpay);
                     } else if(salesMonth === '08'){
-                        augSales.push(data[i].s_downpay + data[i].p_downpay);
+                        augSales.push(data[i].s_downpay);
                     } else if(salesMonth === '09'){
-                        sepSales.push(data[i].s_downpay + data[i].p_downpay);
+                        sepSales.push(data[i].s_downpay);
                     } else if(salesMonth === '10'){
-                        octSales.push(data[i].s_downpay + data[i].p_downpay);
+                        octSales.push(data[i].s_downpay);
                     } else if(salesMonth === '11'){
-                        novSales.push(data[i].s_downpay + data[i].p_downpay);
+                        novSales.push(data[i].s_downpay);
                     } else if(salesMonth === '12'){
-                        decSales.push(data[i].s_downpay + data[i].p_downpay);
+                        decSales.push(data[i].s_downpay);
                     }
                 }
 
@@ -216,9 +222,107 @@ const dashboard = (req, res) => {
             };
             let monthlyChartData = getMonthlyChartData()
             //Monthly Sales Chart Data End
+
+            //Anual Sales Chard Data (until 2040)
+            let getAnnualData = () => {
+                // total sales per month
+                let sales2021 = [0], 
+                    sales2022 = [0], 
+                    sales2023 = [0], 
+                    sales2024 = [0], 
+                    sales2025 = [0], 
+                    sales2026 = [0], 
+                    sales2027 = [0], 
+                    sales2028 = [0], 
+                    sales2029 = [0], 
+                    sales2030 = [0],
+                    sales2031 = [0],
+                    sales2032 = [0],
+                    sales2033 = [0],
+                    sales2034 = [0],
+                    sales2035 = [0],
+                    sales2036 = [0],
+                    sales2037 = [0],
+                    sales2038 = [0],
+                    sales2039 = [0],
+                    sales2040 = [0];
+                    
+                    //to append data for each year
+                for(let i = 0; i < data.length; i++){  
+                    let salesYear = data[i].job_date.substring(0, 4);
+                    
+                    if(salesYear === '2021'){
+                        sales2021.push(data[i].s_downpay);
+                    } else if(salesYear === '2022'){
+                        sales2022.push(data[i].s_downpay);
+                    } else if(salesYear === '2023'){
+                        sales2023.push(data[i].s_downpay);
+                    } else if(salesYear === '2024'){
+                        sales2024.push(data[i].s_downpay);
+                    } else if(salesYear === '2025'){
+                        sales2025.push(data[i].s_downpay);
+                    } else if(salesYear === '2026'){
+                        sales2026.push(data[i].s_downpay);
+                    } else if(salesYear === '2027'){
+                        sales2027.push(data[i].s_downpay);
+                    } else if(salesYear === '2028'){
+                        sales2028.push(data[i].s_downpay);
+                    } else if(salesYear === '2029'){
+                        sales2029.push(data[i].s_downpay);
+                    } else if(salesYear === '2030'){
+                        sales2030.push(data[i].s_downpay);
+                    } else if(salesYear === '2031'){
+                        sales2031.push(data[i].s_downpay);
+                    } else if(salesYear === '2032'){
+                        sales2032.push(data[i].s_downpay);
+                    } else if(salesYear === '2033'){
+                        sales2033.push(data[i].s_downpay);
+                    } else if(salesYear === '2034'){
+                        sales2034.push(data[i].s_downpay);
+                    } else if(salesYear === '2035'){
+                        sales2035.push(data[i].s_downpay);
+                    } else if(salesYear === '2036'){
+                        sales2036.push(data[i].s_downpay);
+                    } else if(salesYear === '2037'){
+                        sales2037.push(data[i].s_downpay);
+                    } else if(salesYear === '2038'){
+                        sales2038.push(data[i].s_downpay);
+                    } else if(salesYear === '2039'){
+                        sales2039.push(data[i].s_downpay);
+                    } else if(salesYear === '2040'){
+                        sales2040.push(data[i].s_downpay);
+                    } 
+                }
+                return sales = {
+                    sales2021: sales2021.reduce((x, y) => x + y),
+                    sales2022: sales2022.reduce((x, y) => x + y),
+                    sales2023: sales2023.reduce((x, y) => x + y),
+                    sales2024: sales2024.reduce((x, y) => x + y),
+                    sales2025: sales2025.reduce((x, y) => x + y),
+                    sales2026: sales2026.reduce((x, y) => x + y),
+                    sales2027: sales2027.reduce((x, y) => x + y),
+                    sales2028: sales2028.reduce((x, y) => x + y),
+                    sales2029: sales2029.reduce((x, y) => x + y),
+                    sales2030: sales2030.reduce((x, y) => x + y),
+                    sales2031: sales2031.reduce((x, y) => x + y),
+                    sales2032: sales2032.reduce((x, y) => x + y),
+                    sales2033: sales2033.reduce((x, y) => x + y),
+                    sales2034: sales2034.reduce((x, y) => x + y),
+                    sales2035: sales2035.reduce((x, y) => x + y),
+                    sales2036: sales2036.reduce((x, y) => x + y),
+                    sales2037: sales2037.reduce((x, y) => x + y),
+                    sales2038: sales2038.reduce((x, y) => x + y),
+                    sales2039: sales2039.reduce((x, y) => x + y),
+                    sales2040: sales2040.reduce((x, y) => x + y)
+                }
+            };
+            //Anual Sales Chard Data (until 2040) End
+
+            let annualChartData = getAnnualData()
+            console.log('annualChartData', annualChartData)
             
             //response object
-            res.render('jobOrder/job-orders', {title: 'Dashboard', jobOrders: data, flashMessage: req.flash('message'), monthNowName,  totalDownPay, totalSalesDownPay, totalRevenue, totalJobOrders, currentOrdersCount, currentPartsOrdersCount, currentUnclaimedOrdersCount, currentDMDOrdersCount, monthlyChartData});
+            res.render('jobOrder/job-orders', {title: 'Dashboard', jobOrders: data, flashMessage: req.flash('message'), monthNowName,  totalDownPay, totalSalesDownPay, totalRevenue, totalJobOrders, currentOrdersCount, currentPartsOrdersCount, currentUnclaimedOrdersCount, currentDMDOrdersCount, monthlyChartData, annualChartData});
         })
         .catch(err => console.log("error from the controller" + err));
 }
